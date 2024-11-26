8ecd727a2aaf5bc1ff25be8984dd316bd9fa33bd
Akshay Ghule <aghule005@gmail.com>
2024-11-27 02:58:16 +0530

    Your commit message

--- src/coponents/Hero.jsx
+++ src/coponents/Hero.jsx
@@ -8,6 +8,8 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
+            
+            <motion.h1 className="pb-16 text-4xl font-thin tracking-tight lg:mt-16 lg:text-5xl">
               Ujjwala Nimbolkar
             </motion.h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
